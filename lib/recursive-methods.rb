89ac35ab981a436400cdb02843d3296e8ba136db
Anna Kim <anna.kim93@gmail.com>
2020-10-18 00:35:53 -0400

    completed digit_match

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -12,7 +12,7 @@ end
 # Time complexity: O(n); an operation is completed for each stack frame
 # Space complexity: O(n^2)
 # >> system stack will include as many stack frames as there are letters in the string
-# >> we create a new string that is (n-1) length each time recursive case is hit
+# >> we create a new string that is (n-i) length each time recursive case is hit; where i is the stack frame level
 def reverse(s)
     # raise NotImplementedError, "Method not implemented"
     return s if s.empty? || s.length == 1
@@ -72,8 +72,19 @@ def is_palindrome(s, low = 0, high = s.length - 1)
     is_palindrome(s, low + 1, high - 1)
 end
 
-# Time complexity: ?
-# Space complexity: ?
-def digit_match(n, m)
-    raise NotImplementedError, "Method not implemented"
+# Time complexity: O(min(n,m)); will do n or m comparisons depending on which number has more digits
+# Space complexity: O(min(n,m))
+# >> n for creation of string;
+# >> m for creation of string;
+# >> min(n, m) stack frames added to system stack
+# >> 2n + m or 2m + n
+def digit_match(n, m, search_idx = -1, count = 0)
+    # raise NotImplementedError, "Method not implemented"
+    n = n.to_s if n.class != String
+    m = m.to_s if m.class != String
+
+    return count if search_idx + n.length < 0 || search_idx + m.length < 0
+
+    n[search_idx] == m[search_idx] ? digit_match(n, m, search_idx - 1, count + 1) : digit_match(n, m, search_idx - 1, count)
 end
+
