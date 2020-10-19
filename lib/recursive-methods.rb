e525ef9e1a6c2cbdb18c1a38b6fa72df87b09785
roshni-patel <rpatel01@wesleyan.edu>
2020-10-19 13:56:15 -0500

    Added explanations for time and space complexity

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -1,7 +1,7 @@
 # Authoring recursive algorithms. Add comments including time and space complexity for each method.
 
-# Time complexity: O(n)
-# Space complexity: O(n)
+# Time complexity: O(n) because n number of operations are needed
+# Space complexity: O(n) because there are n number of recursive calls
 def factorial(n)
     raise ArgumentError, "Cannot calculate the factorial for a negative number" if n < 0
 
@@ -12,8 +12,8 @@ def factorial(n)
     end
 end
 
-# Time complexity: O(n)
-# Space complexity: O(n^2)
+# Time complexity: O(n) because n number of operations are needed, where n is the length of the string
+# Space complexity: O(n^2) because with each recursive call a new string is created
 def reverse(s)
     if s.length <= 1
         return s
@@ -22,8 +22,8 @@ def reverse(s)
     end
 end
 
-# Time complexity: O(n)
-# Space complexity: O(n)
+# Time complexity: O(n) because n number of operations are needed, where n is the length of the string divided by 2
+# Space complexity: O(n) because reversing in place does not create a new string with each recursive call
 def reverse_helper(s, left = 0, right = s.length - 1)
     if left < right
         s[left], s[right] = s[right], s[left]
@@ -38,8 +38,8 @@ def reverse_inplace(s)
     return reverse_helper(s)
 end
 
-# Time complexity: O(n)
-# Space complexity: O(n)
+# Time complexity: O(n) because n number of operations are needed
+# Space complexity: O(n) because there are n number of recursive calls
 def bunny(n)
     if n == 0
         return n
@@ -48,8 +48,8 @@ def bunny(n)
     end
 end
 
-# Time complexity: O(n)
-# Space complexity: O(n^2)
+# Time complexity: O(n) because n number of operations are needed, where n is the length of the string divided by 2
+# Space complexity: O(n^2) because with each recursive call a new string is created
 def nested(s)
     if s.empty?
         return true
@@ -60,8 +60,8 @@ def nested(s)
     end
 end
 
-# Time complexity: O(n)
-# Space complexity: O(n^2)
+# Time complexity: O(n) because n number of operations are needed, where n is the length of the array
+# Space complexity: O(n^2) because with each recursive call a new array is created
 def search(array, value)
     if array.empty?
         return false
@@ -72,8 +72,8 @@ def search(array, value)
     end
 end
 
-# Time complexity: O(n)
-# Space complexity: O(n^2)
+# Time complexity: O(n) because n number of operations are needed, where n is the length of the string divided by 2
+# Space complexity: O(n^2) because with each recursive call a new string is created
 def is_palindrome(s)
     if s.length <= 1
         return true
@@ -84,8 +84,8 @@ def is_palindrome(s)
     end
 end
 
-# Time complexity: O(n)
-# Space complexity: O(n)
+# Time complexity: O(n) because n number of operations are needed, where n is the smaller number
+# Space complexity: O(n) because no additional space is required outside of the recursive calls placed on the stack
 def digit_match(n, m)
     if n <= 1 || m <= 1
         return n == m ? 1 : 0
@@ -96,8 +96,8 @@ def digit_match(n, m)
     end
 end
 
-# Time Complexity: O(2^n)
-# Space Complexity: O(n)
+# Time Complexity: O(2^n) because 2^n number of operations are needed, as each recursive call requires fib(n-1) and fib(n-2)
+# Space Complexity: O(n) because no additional space is required outside of the recursive calls placed on the stack
 def fib(n)
     raise ArgumentError, "Number must be greater than or equal to 0" if n < 0
     if n == 0
