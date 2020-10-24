82743a0e3d1c3c53533d7ea928222dd048902f00
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-10-23 23:38:56 -0700

    add fib method and add the explanations

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -1,7 +1,7 @@
 # Authoring recursive algorithms. Add comments including time and space complexity for each method.
 
 # Time complexity: O(n), it runs n times and depends on how big n is.
-# Space complexity: O(n), the call stack temporarily stores information about the running program, which depends on how big n is, until the method reaches the base case. 
+# Space complexity: O(n), call stack temporarily stores information about the running program, which depends on how big n is, until the method reaches the base case. 
 def factorial(n)
   raise ArgumentError.new("#{n} should be larger than 0") if n < 0
   if n == 0
@@ -13,7 +13,7 @@ def factorial(n)
 end
 
 # Time complexity: O(n), it runs n/2 times which depends on the length of the string
-# Space complexity: O(n), the call stack temporarily saves the return values s[-1] and s[0] until the method reaches the base case. So the space complexity is O(n) which depends on the length of the String.
+# Space complexity: O(n), call stack temporarily saves the return values s[-1] and s[0] until the method reaches the base case. So the space complexity is O(n) which depends on the length of the String.
 def reverse(s)
   if s.length < 2
     return s
@@ -24,7 +24,7 @@ def reverse(s)
 end
 
 # Time complexity: O(n), it runs n/2 times which depends on the length of the string
-# Space complexity: O(n), the call stack temporarily saves the return values s[0] and s[-1] until the method reaches the base case. So the space complexity is O(n) which depends on the length of the String.
+# Space complexity: O(n), call stack temporarily saves the return values s[0] and s[-1] until the method reaches the base case. So the space complexity is O(n) which depends on the length of the String.
 def reverse_inplace(s)
   if s.length < 2
     return s
@@ -36,7 +36,7 @@ def reverse_inplace(s)
 end
 
 # Time complexity: O(n), it runs n times and depends on how big n is.
-# Space complexity: O(n), the call stack temporarily stores information about the running program, which depends on how big n is, until the method reaches the base case. 
+# Space complexity: O(n), call stack temporarily stores information about the running program, which depends on how big n is, until the method reaches the base case. 
 def bunny(n)
   if n == 0
     return 0
@@ -46,8 +46,8 @@ def bunny(n)
   # raise NotImplementedError, "Method not implemented"
 end
 
-# Time complexity: O(n), it runs n/2 times which depends on the length of the string
-# Space complexity: O(1), it's a tail recursion and no extra info is saved in call stack before it reaches to the base case if it's optimized.
+# Time complexity: O(n), it runs n/2 times which depends on the length of the string.
+# Space complexity: O(1)/O(n), it's a tail recursion and no extra info is saved in call stack before it reaches to the base case if it's optimized. Otherwise, because it runs n/2 times, call stack would temporarily saves the return values. It would be O(n).
 def nested(s)
   if s.length == 0
     return true
@@ -60,7 +60,7 @@ def nested(s)
 end
 
 # Time complexity: O(n), it goes thru the whole array to find whether the value is in the array or not. So it depends on the length of array.
-# Space complexity: O(1), it's a tail recursion and no extra info is saved in call stack before it reaches to the base case if it's optimized.
+# Space complexity: O(1)/O(n), it's a tail recursion and no extra info is saved in call stack before it reaches to the base case if it's optimized. Otherwise, because it runs thru the whole array, call stack would temporarily saves the return values. It would be O(n).
 def search(array, value)
   if value == array[0]
     return true
@@ -72,8 +72,8 @@ def search(array, value)
   # raise NotImplementedError, "Method not implemented"
 end
 
-# Time complexity: O(n), it goes thru the whole string to check whether it is palindrome. So it depends on the length of string.
-# Space complexity: O(1), it's a tail recursion and no extra info is saved in call stack before it reaches to the base case if it's optimized.
+# Time complexity: O(n), it runs n/2 times which depends on the length of the string to check whether it is palindrome. So it depends on the length of string.
+# Space complexity: O(1)/O(n), it's a tail recursion and no extra info is saved in call stack before it reaches to the base case if it's optimized. Otherwise, because it runs thru the whole string, call stack would temporarily saves the return values. It would be O(n).
 def is_palindrome(s)
   if s.length == 0
     return true
@@ -86,7 +86,7 @@ def is_palindrome(s)
 end
 
 # Time complexity: O(n), it goes thru the length of the larger number to check the pair of digits. So it depends on the length of the larger number.
-# Space complexity: O(n), the call stack temporarily saves the return values 0 or 1 until the method reaches the base case. So the space complexity is O(n) which depends on the length of the larger number.
+# Space complexity: O(n), call stack temporarily saves the return values 0 or 1 until the method reaches the base case. So the space complexity is O(n) which depends on the length of the larger number.
 def digit_match(n, m)
   if n.to_s.length == 0 || m.to_s.length == 0
     return 0
@@ -95,5 +95,20 @@ def digit_match(n, m)
   else
     return 0 + digit_match(n.to_s[0...-1], m.to_s[0...-1])
   end  
+  # raise NotImplementedError, "Method not implemented"
+end
+
+
+# optional - fibonacci number
+# Time complexity: O(n), it runs n times to find the fibonacci number, so it depends on how big n is.
+# Space complexity: O(n), call stack temporarily saves the return values fib(n-1) and fib(n-2) until the method reaches the base case. So the space complexity is O(n) which depends on how big n is.
+def fib(n)
+  if n == 0
+    return 0
+  elsif n == 1
+    return 1
+  else
+    return fib(n-1) + fib(n-2)
+  end
   # raise NotImplementedError, "Method not implemented"
 end
