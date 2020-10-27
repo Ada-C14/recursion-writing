423a29a2ba73d677fbd849ee88e84905aab0c4b1
Yingjing Lin <yjlin789@gmail.com>
2020-10-27 00:07:05 -0700

    wave 1-5

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -1,33 +1,46 @@
 # Authoring recursive algorithms. Add comments including time and space complexity for each method.
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
 def factorial(n)
-    raise NotImplementedError, "Method not implemented"
+    raise ArgumentError if n < 0
+    return 1 if n == 0
+    return n * factorial(n-1)
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
 def reverse(s)
-    raise NotImplementedError, "Method not implemented"
+    return s if s.length <= 1
+    return s[-1] + reverse(s[1..-2]) + s[0]
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
 def reverse_inplace(s)
-    raise NotImplementedError, "Method not implemented"
+    return reverse_inplace_internal(s, 0)
 end
 
-# Time complexity: ?
-# Space complexity: ?
+def reverse_inplace_internal(s, level)
+    return s if s.length - level * 2 <= 1
+    s[level], s[-1 - level] = s[-1 - level], s[level]
+    return reverse_inplace_internal(s, level + 1)
+end
+
+# Time complexity: O(n)
+# Space complexity: O(n)
 def bunny(n)
-    raise NotImplementedError, "Method not implemented"
+    return 0 if n == 0
+    return 2 if n == 1
+    return 2 + bunny(n-1)
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def nested(s)
-    raise NotImplementedError, "Method not implemented"
+    return false if s.length % 2 == 1
+    return true if s == ""
+    return s[0] != s[-1] && nested(s[1..-2])
 end
 
 # Time complexity: ?
