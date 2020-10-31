8d8308bdc5690930af59a79f426abaec69488889
gessica <gessicamohr18@gmail.com>
2020-10-30 21:13:10 -0700

    wrote the first 5 recursion problems with passing tests

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -3,47 +3,67 @@
 # Time complexity: ?
 # Space complexity: ?
 def factorial(n)
-    raise NotImplementedError, "Method not implemented"
+
+    raise ArgumentError if n < 0
+    return 1 if n == 0
+    return n * factorial(n - 1)
+
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def reverse(s)
-    raise NotImplementedError, "Method not implemented"
+    if s.length <= 1
+        return s
+    end
+
+    return s[-1] + reverse(s[1..-2]) + s[0]
 end
+
 
 # Time complexity: ?
 # Space complexity: ?
-def reverse_inplace(s)
-    raise NotImplementedError, "Method not implemented"
+def reverse_inplace(s, i = 0, j = s.length - 1)
+    return s if s.length == 0 || s.length == 1
+    return s if i >= j
+    temp = s[i]
+    s[i] = s[j]
+    s[j] = temp
+
+    return reverse_inplace(s, i + 1, j - 1)
 end
+
 
 # Time complexity: ?
 # Space complexity: ?
 def bunny(n)
-    raise NotImplementedError, "Method not implemented"
+    return 0 if n <= 0
+    return 2 + bunny(n - 1)
 end
 
 # Time complexity: ?
 # Space complexity: ?
-def nested(s)
-    raise NotImplementedError, "Method not implemented"
+def nested(s, i = 0, j = s.length - 1)
+    return true if s.empty? || i > j && s[i] != s[j]
+    return false if s.length == 1 || s[i] == s[j]
+    return nested(s, i + 1, j - 1)
+
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def search(array, value)
-    raise NotImplementedError, "Method not implemented"
+
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def is_palindrome(s)
-    raise NotImplementedError, "Method not implemented"
+
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def digit_match(n, m)
-    raise NotImplementedError, "Method not implemented"
+
 end
