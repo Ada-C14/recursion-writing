eba32a0681946fdcef50df83a8e7274ee460fe2c
Ada <ada@Adas-MacBook-Pro.local>
2020-11-02 22:38:36 -0500

    7. is_palindrome

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -104,7 +104,13 @@ end
 # Time complexity: ?
 # Space complexity: ?
 def is_palindrome(s)
-    raise NotImplementedError, "Method not implemented"
+    if s.length < 2
+        return true
+    elsif s[0] != s[-1]
+        return false
+    else
+        return is_palindrome(s[1..-2])
+    end
 end
 
 # Time complexity: ?
