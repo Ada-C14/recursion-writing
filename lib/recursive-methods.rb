49d402dc68adb38318ad26d267ef877e83c61eb1
Sophie Messing <sophie.e.messing@gmail.com>
2020-11-04 14:01:56 -0800

    reverse inplace

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -18,8 +18,16 @@ end
 # Time complexity: O(n^2)
 # Space complexity: O(n^2)
 def reverse_inplace(s)
-    return s if s.length <= 1 # base case
-    return s[-1] + reverse(s[1..-2]) + s[0] #recursive case
+    return reverse_in_place_helper(s, 0, -1)
+end
+
+def reverse_in_place_helper(s, first, last)
+    if first >= last
+        return ""
+    else
+        s[first], s[last] = s[last], s[first]
+        return reverse_in_place_helper(s, first + 1, last - 1)
+    end
 end
 
 # Time complexity: O(n^2)
@@ -54,8 +62,8 @@ def is_palindrome(s)
     return is_palindrome(s[1..-2])
 end
 
-# Time complexity: O(n)
-# Space complexity: O(n)
+# Time complexity: O(n^2)
+# Space complexity: O(n^2)
 
 def digit_match(n, m, count = 0)
 
