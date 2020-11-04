0bc7d40d52fc09d78d478c5fdbdd2c75e65e1ec8
Hanh Solo <minhhanhnyc@gmail.com>
2020-11-03 21:55:35 -0800

    Updated factorial

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -3,8 +3,10 @@
 # Time complexity: O(n)
 # Space complexity: O(n)
 def factorial(n)
-  if n == 1
-    return n
+  if n < 0
+    raise ArgumentError
+  elsif n <= 1 
+    return 1
   else
     n * factorial(n - 1)
   end
