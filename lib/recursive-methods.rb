68883d91914c4df325a7e5d1dbfdeccb2f08aef4
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-11-11 10:55:20 -0800

    Newly revised and complete recursion writing homework

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -18,11 +18,20 @@ def reverse(s, i = 1)
     return reversed
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) -still has to iterate over every element in string
+# Space complexity: O(n) -stack can get very large if string is big
+def reverse_inplace_helper(s, start, finish)
+    if start < finish
+        s[start], s[finish] = s[finish], s[start]
+        return reverse_inplace_helper(s, start + 1, finish - 1)
+    else
+        return s
+    end
+end
+
+
 def reverse_inplace(s)
-    return s if s == "" || s.length < 2
-    return s[-1] + reverse_inplace(s[1..-2]) + s[0]
+    return reverse_inplace_helper(s, 0, s.length - 1)
 end
 
 
@@ -33,8 +42,8 @@ def bunny(n)
     return 2 + bunny(n - 1)
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
 def nested(s)
     return true if s == "" || s.length == 0
     return false if s.length.odd?
@@ -44,8 +53,8 @@ def nested(s)
         return false
     end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) best case O(1) if item is in index 1?
+# Space complexity: O(n) best case O(1) if item is in index 1?
 def search(array, value, i = 0)
     return true if array[i] == value
     return false if i >= array.length
@@ -53,16 +62,24 @@ def search(array, value, i = 0)
 
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) - traversing the whole string
+# Space complexity: O(n) -traversing the whole string
 def is_palindrome(s)
     return true if s == "" || s.length < 2
     return false if s[0] != s[-1]
         return is_palindrome(s[1..-2])
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
+def digit_helper(n, m, match = 0, place = 0)
+    return 1 if n == 0 && m == 0 && place == 0
+    return match if n == 0 || m == 0
+    match += 1 if n % 10 == m % 10
+    return digit_helper(n/10, m/10, match, place + 1)
+    return match
+end
+
 def digit_match(n, m)
-    puts "Sorry, I couldn't figure this one out!"
+    return digit_helper(n, m, 0,  0)
 end
