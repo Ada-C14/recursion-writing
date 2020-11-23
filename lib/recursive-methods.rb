742e9a31ab253bdb0ff2014a92345449875ef2da
Blaine <b.atsbeha@gmail.com>
2020-11-23 02:50:08 -0800

    W.I.P.

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -1,27 +1,52 @@
 # Authoring recursive algorithms. Add comments including time and space complexity for each method.
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: 0(n)
 def factorial(n)
-    raise NotImplementedError, "Method not implemented"
+
+    if n < 0
+        raise ArgumentError
+    elsif n == 0
+        return 1
+    else
+        return n * factorial(n-1)
+    end
+
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: ? O(n)
+# Space complexity: O(n)^2
 def reverse(s)
-    raise NotImplementedError, "Method not implemented"
+    # raise ArgumentError
+    if s.length == 0 || s.length == 1
+        return s
+    else
+        return s[-1] + reverse(s[0...-1])
+    end
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
 def reverse_inplace(s)
-    raise NotImplementedError, "Method not implemented"
+    # raise NotImplementedError, "Method not implemented"
+    return my_first_rapper(s, index = 0, last = -1)
 end
+
+def my_first_rapper(s, index = 0, last = -1)
+    if index >= s.length/2 || s.length <= 1
+        return s
+    else
+        s[index], s[last] = s[last], s[index]
+        my_first_rapper(s, index + 1, last - 1)
+    end
+end
+
 
 # Time complexity: ?
 # Space complexity: ?
 def bunny(n)
-    raise NotImplementedError, "Method not implemented"
+    # raise NotImplementedError, "Method not implemented"
+
 end
 
 # Time complexity: ?
