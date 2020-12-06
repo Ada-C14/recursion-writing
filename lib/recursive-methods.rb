67e8096b829a1cda5c0e32137302838308245117
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2020-12-05 20:00:47 -0800

    added Factorial, Reverse, Reverse in place, Bunny and palindrome

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -1,15 +1,27 @@
 # Authoring recursive algorithms. Add comments including time and space complexity for each method.
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: o(n)
+# Space complexity: o(n)
 def factorial(n)
-    raise NotImplementedError, "Method not implemented"
+
+    if n == 0 || n == 1
+        return 1
+    elsif n < 0
+        raise ArgumentError, " Enter a number greater than 0."
+    else
+        return n * factorial(n - 1)
+    end
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: o(n) - N being the lenght of string
+# Space complexity: o(n) - we would need n amount of space for each character in string
 def reverse(s)
-    raise NotImplementedError, "Method not implemented"
+    if s.length <= 1
+        return s
+    else
+        return reverse(s[1..-1]) + s[0]
+    end
+
 end
 
 # Time complexity: ?
@@ -18,10 +30,16 @@ def reverse_inplace(s)
     raise NotImplementedError, "Method not implemented"
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: o(n)
+# Space complexity: o(n
 def bunny(n)
-    raise NotImplementedError, "Method not implemented"
+    return n if n == 0
+
+    if n == 1
+        return 2
+    else
+        return 2 + bunny(n - 1)
+    end
 end
 
 # Time complexity: ?
@@ -36,10 +54,16 @@ def search(array, value)
     raise NotImplementedError, "Method not implemented"
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: o(n)
+# Space complexity: o(n)
 def is_palindrome(s)
-    raise NotImplementedError, "Method not implemented"
+    if s.length <= 1
+        return true
+    elsif s[0] != s[-1]
+        return false
+    else
+        return is_palindrome(s[1..-2])
+    end
 end
 
 # Time complexity: ?
