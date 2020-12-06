32a62aeb13bf518280b1c69dcfb5e091b6401d30
Madeline <madelign@gmail.com>
2020-12-05 19:27:35 -0500

    added factorial, reverse_inplace, bunny, and nested methods.

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -1,33 +1,74 @@
 # Authoring recursive algorithms. Add comments including time and space complexity for each method.
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: o(n)
+# Space complexity: o(n)
 def factorial(n)
-    raise NotImplementedError, "Method not implemented"
+    return 1 if n == 1
+
+    if n < 0
+        raise ArgumentError.new("n must be >=0")
+    end
+
+    return n * factorial(n-1)
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def reverse(s)
-    raise NotImplementedError, "Method not implemented"
+
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def reverse_inplace(s)
-    raise NotImplementedError, "Method not implemented"
+    return reverse_inplace_recursive(s, 0)
+end
+
+def reverse_inplace_recursive(s, index)
+
+    if s.length / 2 == index
+        return s
+    end
+
+    temp_char = s[index]
+    s[index] = s[s.length - 1 - index]
+    s[s.length - 1 - index] = temp_char
+
+    # alternative way for the same thing
+    # temp_char =  s[s.length - 1 - index]
+    # s[s.length - 1 - index] = s[index]
+    # s[index] = temp_char
+
+    return reverse_inplace_recursive(s, index + 1)
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def bunny(n)
-    raise NotImplementedError, "Method not implemented"
+    if n == 0
+        return n
+    end
+
+    return 2 + bunny(n - 1)
 end
 
 # Time complexity: ?
 # Space complexity: ?
-def nested(s)
-    raise NotImplementedError, "Method not implemented"
+def nested(s, index = 0)
+    if s.length % 2 == 1
+        return false
+    end
+
+    if s.length / 2 == index
+        return true
+    end
+
+    if s[index] == s[s.length - 1 - index]
+        return false
+    end
+
+    return nested(s, index + 1)
+
 end
 
 # Time complexity: ?
